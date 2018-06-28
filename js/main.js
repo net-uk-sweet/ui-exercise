2bf7dfc6c334d8d39bdb69494d12b50750fd4df2
Ian Watson <ian@sweet.uk.net>
2018-06-28 23:41:20 +0100

    Exercise 3

--- js/main.js
+++ js/main.js
@@ -1,1 +1,28 @@
-window.console.log('hello world');
+var toggle = (function ($) {
+
+    'use strict';
+
+    var $trigger,
+        $target;
+
+    var visibleClass = 'is-visible';
+
+    return {
+        init: function () {
+            this.$trigger = $('[data-toggle-trigger]');
+            this.$target = $('[data-toggle-target]');
+
+            this.$trigger.on('click', this.toggle.bind(this));
+        },
+
+        toggle: function (e) {
+            if (e) {
+                e.preventDefault();
+            }
+
+            this.$target.toggleClass(visibleClass);
+        }
+    }
+}(jQuery));
+
+toggle.init();
